3abd268ad8ea5a44c616011af314e9b6cc257684
ketjehdev <ketjeh.developer@gmail.com>
2022-05-25 00:04:16 +0700

    fix user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'password',
+        'role',
     ];
 
     /**
@@ -30,15 +31,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
